121d57defdff1f1f92bc10fa4c719b9303e10274
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-04 02:24:09 +0300

    Update StandardGradeBook.cs

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,7 +1,7 @@
 using GradeBook.Enums;
 namespace GradeBook.GradeBooks
 {
-    public class StandardGradeBook : BaseGradeBook
+    public class StandardGradeBook :BaseGradeBook
     {
         public StandardGradeBook(string name) : base(name)
         {
