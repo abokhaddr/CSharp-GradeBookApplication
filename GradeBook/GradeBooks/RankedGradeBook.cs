61e1462542ae0b70ca3ea63e0a77a33e59e21d57
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-04 02:01:00 +0300

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,1 +1,12 @@
-
+using System;
+using GradeBook.Enums;
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
