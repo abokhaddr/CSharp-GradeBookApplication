1714339a99c21e53b96288ced9fd391d5dc17017
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-05 01:06:04 +0300

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,20 +11,6 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
-        public override char GetLetterGrade(double averageGrade)
-        {
 
-            if (Students.Count< 5)
-            {
-                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work)");
-            }
-
-            var threshold = (int) Math.Ceiling (Students.Count * 0.2); // Get the 20% 
-            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-
-
-            //return base.GetLetterGrade(averageGrade);
-            return 'F';
-        }  }
     }
 }
