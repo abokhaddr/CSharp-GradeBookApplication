4912aa249fb1485821f20fd83e9fcfd726323621
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-05 03:26:20 +0300

    Update StandardGradeBook.cs

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,12 +1,1 @@
-using GradeBook.Enums;
 
- namespace GradeBook.GradeBooks
-{
-     public class StandardGradeBook : BaseGradeBook
-    {
-      public   StandardGradeBook(string name) : base(name)
-        {
-            Type = GradeBookType.Standard;
-        }  
-    }
-}
