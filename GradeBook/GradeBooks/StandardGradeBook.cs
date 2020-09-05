399a4dce5bff66e9378180b1f6367504129c3ecc
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-05 03:48:18 +0300

    Update StandardGradeBook.cs

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,1 +1,12 @@
-
+using System;
+using GradeBook.Enums;
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook: BaseGradeBook
+    {
+        public StandardGradeBook(string name): base (name) // We're calling the constructore of the base class.
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
