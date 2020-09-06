f5ff870c42c02b09fff8b29f9ca2d21ffdae178c
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-06 23:17:29 +0300

    Update StandardGradeBook.cs

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+
 namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name) : base(name)
-        {
-        Name = name;
-            Type = Enums.GradeBookType.Standard;
+        public StandardGradeBook(string name, bool isWeighted)
+            : base(name, isWeighted) {
+            this.Type = Enums.GradeBookType.Standard;
         }
     }
 }
