50a4a03cc15a031ef50a15d0dcf5b815c864de05
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-11 01:01:27 +0300

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -31,14 +31,17 @@ namespace GradeBook.UserInterfaces
                 Console.WriteLine("{0} was not recognized, please try again.", command);
         }
 
-        public static void CreateCommand(string command)
+          public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
             if (parts.Length != 2)
+            if (parts.Length != 3)
             {
                 Console.WriteLine("Command not valid, Create requires a name.");
                 return;
-            }
+        
+        
+        
             var name = parts[1];
             BaseGradeBook gradeBook = new BaseGradeBook(name);
             Console.WriteLine("Created gradebook {0}.", name);
